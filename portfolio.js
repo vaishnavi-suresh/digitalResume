a9593ba2216fdd9f03112f695a9a01e0be952820
Vaishnavi <vs2753@nyu.edu>
2024-09-03 00:29:14 -0400

    finish navigation functionality experiences section

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let tileOne = document.getElementsByClassName("one")[0];
+    let tileTwo = document.getElementsByClassName("two")[0];
+    let tileThree = document.getElementsByClassName("three")[0];
+    let tileFour = document.getElementsByClassName("four")[0];
+    let tileFive = document.getElementsByClassName("five")[0];
+    tileFour.style.display = "none";
+    tileFive.style.display = "none";
+
+});
